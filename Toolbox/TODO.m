be6806a1e0964939ef91d30f9f11260e7852617c
leonardo <31987224+leom97@users.noreply.github.com>
2022-09-13 09:56:56 +0200

    Delete TODO.m

--- Toolbox/TODO.m
+++
@@ -1,6 +0,0 @@
-% Remember to consider residual weighting also in the specular part
-% Note: while computing the residual for the specular part I have neglected
-% such weighting
-% Note, such a residual should be flipped in sign and should be > 0
-
-% Remember to subtract I specular from the total image!
